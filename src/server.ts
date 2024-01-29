9435039b82b92b0b9f489f8bc41c2995bf6cb797
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2024-01-29 11:03:55 -0300

    Base Project

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import https from 'https'
+import http from 'http'
+import siteRoutes from './routes/site'
+import { requestIntercepter } from './utils/requestIntercepter'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.all('*', requestIntercepter)
+
+//app.user('/admin', adminRoutes)
+app.use('/', siteRoutes);
+
+const runServer = (port: number, server: http.Server) => {
+    server.listen(port, () => {
+        console.log(`Running at PORT ${port}`)
+    })
+}
+
+const regularServer = http.createServer(app)
+if(process.env.NODE_ENV === 'production') {
+    //TODO: configurar ssl
+    //TODO: rodar server na 80 e 443
+} else {
+    const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000
+    runServer(serverPort, regularServer)
+}
